e0fd43f645bf11d3f68b6615269d69f01ea9b612
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-08-17 12:45:05 +0530

    Added glow

--- lib/screens/home/section.dart
+++ lib/screens/home/section.dart
@@ -20,37 +20,40 @@ class HomeSection extends StatefulWidget {
 class _HomeSectionState extends State<HomeSection> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: contentPadding,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                widget.heading.toUpperCase(),
-                style: GoogleFonts.workSans(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              Row(
-                children: [
-                  RawMaterialButton(
-                    onPressed: widget.expandAction,
-                    child: Text("View All", style: GoogleFonts.workSans()),
+    return Container(
+      margin: EdgeInsets.only(bottom: 30),
+      child: Column(
+        children: [
+          Container(
+            padding: contentPadding,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  widget.heading.toUpperCase(),
+                  style: GoogleFonts.workSans(
+                    fontSize: 15,
+                    fontWeight: FontWeight.w600,
                   ),
-                ],
-              )
-            ],
+                ),
+                Row(
+                  children: [
+                    RawMaterialButton(
+                      onPressed: widget.expandAction,
+                      child: Text("View All", style: GoogleFonts.workSans()),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
-        ),
-        SingleChildScrollView(
-          padding: contentPadding / 2,
-          scrollDirection: Axis.horizontal,
-          child: widget.items,
-        ),
-      ],
+          SingleChildScrollView(
+            padding: contentPadding / 2,
+            scrollDirection: Axis.horizontal,
+            child: widget.items,
+          ),
+        ],
+      ),
     );
   }
 }
